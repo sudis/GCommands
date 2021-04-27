8e807363409021dc0c1021138f6c2612fd1bd883
HyRo <generalkubo@gmail.com>
2021-04-27 19:24:12 +0200

    1.0.49

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -8,19 +8,6 @@ module.exports = {
         message.channel.send(embed)
       }
 
-      client.api.interactions(slash.id, slash.token).callback.post({
-          data: {
-              type: 4,
-              data: await createAPIMessage(slash, embed)
-          }
-      })
-
-      async function createAPIMessage(interaction, content) {
-        const apiMessage = await Discord.APIMessage.create(client.channels.resolve(interaction.channel_id), content)
-          .resolveData()
-          .resolveFiles();
-        
-        return { ...apiMessage.data, files: apiMessage.files };
-      }
+      return embed;
   }
 };
