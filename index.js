a9a02773f5e228b86ede1f09ed31c68d716e8e8e
HyRo <generalkubo@gmail.com>
2021-05-06 14:10:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new Discord.Client();
 client.on("ready", () => {
     new GCommands(client, {
         cmdDir: "commands",
+        eventDir: "events",
         errorMessage: "Error :(", // optional
         slash: {
            slash: 'both', //true = slash only, false = only normal, both = slash and normal
@@ -14,6 +15,10 @@ client.on("ready", () => {
         cooldown: {
             message: "Please wait {cooldown} more second(s) before reusing the \`{cmdname}\` command.",
             default: 3 // default cooldown for all commands
+        },
+        database: {
+            type: "mongodb", //sqlite/mongodb
+            url: "mongodb+srv://" //mongourl
         }
     })
     
