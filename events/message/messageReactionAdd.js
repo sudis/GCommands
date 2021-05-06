9b824582d2f243992206668ee6d162b8be89b0a1
HyRo <generalkubo@gmail.com>
2021-05-06 14:21:17 +0200

    Update messageReactionAdd.js

--- events/message/messageReactionAdd.js
+++ events/message/messageReactionAdd.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "messageReactionAdd",
-    once: false,
+    	once: false,
 	run: async(client, messageReaction, user) => {
-        console.log(`a reaction is added to a message`);
+        	console.log(`a reaction is added to a message`);
 	}
 };
