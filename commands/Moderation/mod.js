e04485506daf1cc9b463704228787906cab1e31b
HyRo <generalkubo@gmail.com>
2021-05-06 14:39:15 +0200

    Update mod.js

--- commands/Moderation/mod.js
+++ commands/Moderation/mod.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "mod cmd",
     expectedArgs: '<enable> <test>',
     subCommandGroup: "use",
-    subCommand: ["ban;<id>","kick;<id>"],
+    subCommand: ["ban;<user:6:set user>","kick;<user:6:set user>"],
     run: async(client, slash, message, args) => {
       if(message) {
         if(args[0] == "ban") {
@@ -12,7 +12,8 @@ module.exports = {
       }
 
       if(slash.data.options[0].options[0].name == "ban") {
-        const member = client.users.cache.get(slash.data.options[0].options[0].options[0].value)
+           console.log(slash.data)
+          //const member = client.users.cache.get(slash.data.options[0].options[0].options[0].value)
         // code
       }
   }
