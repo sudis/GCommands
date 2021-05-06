e7160da6882f196c2cffe860d3e1b23b3f6a02c4
HyRo <generalkubo@gmail.com>
2021-05-06 14:20:28 +0200

    Create messageReactionRemoveAll.js

---
+++ events/message/messageReactionRemoveAll.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "messageReactionRemoveAll",
+    once: false,
+	run: async(client, message) => {
+        console.error(`all reactions are removed from a message`);
+	}
+};
