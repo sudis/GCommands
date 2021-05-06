112e8969a15632c9672fe5dd2faf2798738d40bb
Šebestíček <44745014+SebestikCZ@users.noreply.github.com>
2021-05-06 13:48:52 +0000

    Create guildMemberUpdate.js

---
+++ events/guild/guildMemberUpdate.js
@@ -0,0 +1,6 @@
+module.exports = {
+	name: "guildMemberUpdate",
+	run: async (oldMember, newMember) => {
+    	console.error(`a guild member changes - i.e. new role, removed role, nickname.`);
+	});
+}
